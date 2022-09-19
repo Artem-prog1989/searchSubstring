dfd1b621087ab5db07c61fcb02474740ad8b3ef0
кабася <@HOMEPC>
2022-09-19 13:39:33 +0300

    Добавьте файлы проекта.

---
+++ searchSubstring/Program.cs
@@ -0,0 +1,19 @@
+﻿string text = "heromeroomerovvkj";
+string substring = "mero";
+int t = 0;
+
+//// поиск индекса вхождения подстроки в строке
+while(t<=(text.Length-substring.Length))
+{
+    int p = 0;
+    while (p<substring.Length && (text[t + p] == substring[p]))
+    {
+        p++;
+    }
+    
+    if (p == (substring.Length))
+    {
+        Console.WriteLine(t);
+    }
+    t++;
+}
